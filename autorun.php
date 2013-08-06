ecb9e29467185deeee3b77850bb7f60e915863a3
Marcos Minond <minond.marcos@gmail.com>
2013-08-05 20:33:42 -0600

    adding autorun file

---
+++ autorun.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace util\error;
+
+require_once 'src/util/error/ErrorOutput.php';
+require_once 'src/util/error/FancyTemplateOutput.php';
+require_once 'src/util/error/NiceError.php';
+
+$niceerror = false;
+
+// TODO: handle cli and json requests
+if (php_sapi_name() !== 'cli') {
+    $headers = getallheaders();
+    $accepts = isset($headers['Accept']) ? $headers['Accept'] : '';
+
+    if (strpos($accepts, 'text/html') !== false) {
+        $niceerror = new NiceError(
+            new FancyTemplateOutput(
+                __DIR__ . '/resources/nice_error.phtml'));
+    }
+}
+
+if ($niceerror) {
+    ini_set('display_errors', false);
+    set_error_handler([ $niceerror, 'handleError' ]);
+    set_exception_handler([ $niceerror, 'handleException' ]);
+    register_shutdown_function([ $niceerror, 'handleShutdown' ]);
+}
+
+return $niceerror;
+
